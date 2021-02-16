919b4fffce9ce2a7c1b83f1af6f9f7ba90cc5666
Enai63 <Toha76@yandex.ru>
2021-02-16 22:13:45 +0400

    InitCommit

---
+++ src/main/java/ru/enai/controller/CompanyController.java
@@ -0,0 +1,59 @@
+package ru.enai.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.enai.exceptions.NotFoundException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/company")
+public class CompanyController {
+    private Long counter = 4L;
+    private List<Map<String, String>> companys = new ArrayList<Map<String, String>>()
+    {{
+        add(new HashMap<String, String>() {{put("id", "1"); put("text", "company1");}});
+        add(new HashMap<String, String>() {{put("id", "2"); put("text", "company2");}});
+        add(new HashMap<String, String>() {{put("id", "3"); put("text", "company3");}});
+    }};
+
+    @GetMapping
+    public List<Map<String, String>>  list() {
+        return companys;
+    }
+
+    @GetMapping("{id}")
+    public Map<String, String> getOne(@PathVariable String id) {
+        return getCompany(id);
+    }
+
+    private Map<String, String> getCompany(String id) {
+        return companys.stream()
+                .filter(company -> company.get("id").equals(id))
+                .findFirst()
+                .orElseThrow(NotFoundException::new);
+    }
+
+    @PostMapping
+    public Map<String, String> create(@RequestBody Map<String, String> company) {
+        company.put("id", String.valueOf(counter++));
+        companys.add(company);
+        return company;
+    }
+
+    @PutMapping("{id}")
+    public Map<String, String> update(@PathVariable String id, @RequestBody Map<String, String> company) {
+        Map<String, String> companyFromDB = getCompany(id);
+        companyFromDB.putAll(company);
+        companyFromDB.put("id", id);
+        return companyFromDB;
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id) {
+        Map<String, String> company = getCompany(id);
+        companys.remove(company);
+    }
+}
